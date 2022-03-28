046aee53fdfb16f9270019845b0b600b65050510
Vladyslav Svitana <vladik719@gmail.com>
2022-03-28 14:02:19 +0200

    starting-changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+import React from 'react';
+
+export const App = () => {
+  return (
+      <>
+      <div className="code__title">
+        <a className="code__link" href="https://github.com/dairedo7/react-assignment-phonebook">
+          Project's original code
+        </a>
+      </div>
+      </>
+  );
+};
