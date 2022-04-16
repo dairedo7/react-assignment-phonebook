a09fad56bf278e82a7f65c7cb8be2f7fbc67762c
Vladyslav Svitana <vladik719@gmail.com>
2022-04-16 20:47:52 +0200

    localStorage with lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,28 @@ import ContactList from './components/ContactList/ContactList.js';
 import Filter from './components/Filter/Filter';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: ''
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts);
+    // console.log(parsedContacts)
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    }
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      console.log("The contacts have been updated!")
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   deleteContact = id => {
     const { state } = this;
