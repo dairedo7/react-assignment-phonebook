4f5e6014ec16952c05ef0eb21f15111f9954f8ab
Vladyslav Svitana <vladik719@gmail.com>
2022-05-01 01:17:28 +0200

    minor_change

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
-    if (prevState !== contacts) {
+    if (prevState.length !== contacts.length) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
   }
